26f96e796f3bd3b1efc80c995fd41baf0d408a09
Rrahmania <rahmaniautami34@gmail.com>
2025-04-19 08:48:58 +0700

    soal2.cpp
    RahmaniaUtami_241401034_UTS_AP1

--- soal2.cpp
+++ soal2.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main(){
+int main() {
     string biner;
     int i, pjg, desimal;
     
-    cout<<"Input Bilangan Biner : ";
+    cout << "Input Bilangan Biner : ";
     getline(cin, biner);
 
     pjg = biner.size();
     desimal = 0;
 
-    for (i = 0; i <pjg;i++)
+    for (i = 0; i < pjg; i++)
         if (biner[i] != '0')
-        desimal += pow(2,pjg-i-1);
+            desimal += (1 << (pjg - i - 1));
 
-        cout << "angka desimal dari biner " << biner << " adalah : " << desimal << endl;
+    cout << "Angka desimal dari biner " << biner << " adalah : " << desimal << endl;
 }
